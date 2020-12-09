2b99772164fe176fdc51c128ea8f6d85dadc51b2
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-09 22:29:56 +0000

    Checkbox that uses useState to manage its state.

--- src/index.js
+++ src/index.js
@@ -3,22 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function App() {
-  const [
-    status, 
-    setStatus
-  ] = useState('Not Delivered');
-
+  const [checked, setChecked] = useState('false');
   return (
     <div>
-      <h1>The package is: {status}.</h1>
-      <button onClick={() => setStatus('Delivered')}>Deliver</button>
+      <input type="checkbox" value={checked} onClick={() => setChecked(!checked)}/>
+      <p>{checked ? 'checked' : 'not checked'}</p>
     </div>
   );
 }
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React>
     <App />
-  </React.StrictMode>,
+  </React>,
   document.getElementById('root')
 );
