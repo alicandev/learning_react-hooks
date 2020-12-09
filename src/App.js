e790f4b4dc19e5484efdf34daa1268a76cd8dfba
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-09 22:12:07 +0000

    Object and array destructuring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ({ name }) => (
+  <div className="App">
+    <h1>Hello {name}!</h1>
+  </div>
+);
+
+App.propTypes = {
+  name: PropTypes.string.isRequired
+};
 
 export default App;
