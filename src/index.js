d4da8692de7cbedd567ffcbd233578578cb93284
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-09 22:54:27 +0000

    Sending interactions up component trees.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { FaStar } from 'react-icons/fa';
@@ -8,22 +8,40 @@ const createArray = (length) => (
   [...Array(length)]
 );
 
-const Star = ({ selected = false })  => (
-  <FaStar color={selected ? 'red' : 'grey'}/>
+const Star = ({ selected = false, onSelect })  => (
+  <FaStar 
+    color={selected ? 'red' : 'grey'}
+    onClick={onSelect}
+  />
 );
 
-const StarRating = ({ totalStars = 5 }) => (
-  createArray(totalStars).map((n,i) => 
-    (<Star key={i} />)
-  )
-);
+const StarRating = ({ totalStars = 5 }) => {
+  const [ selectedStars, setSelectedStars ] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n,i) => 
+        <Star 
+          key={i} 
+          selected={selectedStars > i} 
+          onSelect={() => setSelectedStars(i + 1)} 
+        />
+      )}
+      <p>{selectedStars} of {totalStars}</p>
+    </>
+  );
+};
 
 const App = () => (
   <StarRating totalStars={10} />
 );
 
+// PropTypes
 StarRating.propTypes = {
-  totalStars: PropTypes.number.isRequired
+  totalStars: PropTypes.number
+};
+Star.propTypes = {
+  selected: PropTypes.bool,
+  onSelect: PropTypes.func
 };
 
 ReactDOM.render(
