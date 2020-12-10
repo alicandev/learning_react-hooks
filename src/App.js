7a7ca315dfb7661dc6a2acbd12e80ef96c824fad
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-10 10:45:01 +0000

    Building a fetch component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import './App.css';
-import { useTrees } from './';
+// import React from 'react';
+// import './App.css';
+// import { useTrees } from './';
 
-const App = () => {
-  const {trees} = useTrees();
-  return (
-    <div>
-      <h1>Trees I&apos;ve heard of</h1>
-      <ul>
-        {trees.map(tree => <li key={tree.id}>{tree.type}</li>)} 
-      </ul>
-    </div>
-  );
-};
+// const App = () => {
+//   const {trees} = useTrees();
+//   return (
+//     <div>
+//       <h1>Trees I&apos;ve heard of</h1>
+//       <ul>
+//         {trees.map(tree => <li key={tree.id}>{tree.type}</li>)} 
+//       </ul>
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
-import React, { 
-  createContext,
-  useContext
-} from 'react';
+import React, { } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import useFetch from './useFetch';
+import propTypes from 'prop-types';
 
+const App = ({ login }) => {
+  const { loading, data, error } = useFetch(`http://api.github.com/users/${login}`);
+  console.log(data,error);
 
+  if (loading) return (
+    <h1>Loading...</h1>
+  );
+  if (error) return (
+    <pre>{JSON.stringify(error, null, 2)}</pre>
+  );
+  return (
+    <div>
+      <img src={data.avatar_url} alt={data.login} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+
+  );
+};
+
+App.propTypes = {
+  login: propTypes.string
+};
 
 ReactDOM.render(
-  <TreesContext.Provider value={{trees}}>
-    <App />
-  </TreesContext.Provider>,
+  <React.StrictMode>
+    <App login="alcadeta"/>
+  </React.StrictMode>,
   document.getElementById('root')
 );
