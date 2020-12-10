4c01ec412d6d54becd55d186fb3c79a53ac65642
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-10 10:28:28 +0000

    Data fetching with a fetch hook

--- src/index.js
+++ src/index.js
@@ -6,15 +6,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const TreesContext = createContext();
-export const useTrees = () => useContext(TreesContext);
 
-const trees = [
-  {id: '1', type: 'Maple'},
-  {id: '2', type: 'Oak'},
-  {id: '3', type: 'Family'},
-  {id: '4', type: 'Component'}
-];
 
 ReactDOM.render(
   <TreesContext.Provider value={{trees}}>
