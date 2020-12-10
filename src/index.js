613035953ba778cef96fbc8e056931e77adda11d
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-10 08:10:25 +0000

    Working with the dependency array.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const App = () => {
-  const [name, setName] = useState('Jan');
-  useEffect(() => document.title = `Celebrate ${name}`);
+  const [ name, setName ] = useState('Jan');
+  const [ admin, setAdmin ] = useState(false);
+
+  useEffect(() => {
+    console.log(`Celebrate ${name}`);
+  }, [name]);
+
+  useEffect(() => { 
+    console.log(`the user is: ${admin ? 'admin' : 'not admin'}.`);
+  }, [admin]);
+
   return (
     <section>
       <p>Congratulations {name}!</p>
-      <button onClick={() => setName('Will')}>Change Winner</button>
+      <button onClick={() => setName('Will')} >
+        Change Winner
+      </button>
+      <p>{admin ? 'logged in' : 'not logged in'}</p>
+      <button onClick={() => setAdmin(true)}>
+        Log In
+      </button>
     </section>
   );
 };
